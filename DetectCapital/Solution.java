8d39a76e74e87739010bc619245a5f7de5be187c
unknown <walid.boulima@outlook.com>
2025-01-15 23:28:00 +0100

    LeetCode DetectCapital 15/01/2025

--- DetectCapital/Solution.java
+++ DetectCapital/Solution.java
@@ -11,7 +11,6 @@ class Solution {
         if (Character.isUpperCase(word.charAt(0)) && word.substring(1).equals(word.substring(1).toLowerCase())) {
             return true;
         }
-
         return false;
     }
 }
